a7b1d32372409cd69a963e06e826684172845be9
leehj8896 <leehj8896@gmail.com>
2019-03-28 12:06:29 +0900

    QR코드 read 버튼, 로컬에 있는 DB 가져오는 페이지버튼 추가

---
+++ src/QR_hj/app/src/main/java/com/example/qr_hj/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.qr_hj;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements Button.OnClickListener {
+
+    private  Button read_qr;
+    private Button next_page;
+    private TextView textview;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        read_qr = (Button) findViewById(R.id.read_qr);
+        read_qr.setOnClickListener(this);
+
+        textview = (TextView) findViewById(R.id.textView);
+
+        next_page = (Button) findViewById(R.id.next_page);
+        next_page.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view){
+
+        if(view.getId() == R.id.read_qr)
+            new IntentIntegrator(this).initiateScan();
+
+        if(view.getId() == R.id.next_page){
+            intent = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(intent);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultConde, Intent data){
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultConde, data);
+        if(result != null){
+            //QR코드가 없을 경우
+            if(result.getContents() == null) {
+                Toast.makeText(MainActivity.this, "취소", Toast.LENGTH_SHORT).show();
+            }
+            //QR코드가 있을 경우
+            else {
+                Toast.makeText(MainActivity.this, "스캔완료", Toast.LENGTH_SHORT).show();
+                //json코드
+                try{
+                    JSONObject obj = new JSONObject(result.getContents());
+                    textview.setText(obj.toString());
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                    textview.setText(result.getContents());
+                }
+            }
+        }
+    }
+}
