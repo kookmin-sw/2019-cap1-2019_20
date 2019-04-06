34467538bcbb58b1776cda60afd7530dff75b57b
jiseonpark <plm7234@naver.com>
2019-04-06 17:38:21 +0900

    이벤트 랭킹,참여 버튼추가 및 장소추가 layoutㄹ

--- src/Real_VisitTogether/app/src/main/java/com/example/real_visittogether/Eventregistration.java
+++ src/Real_VisitTogether/app/src/main/java/com/example/real_visittogether/Eventregistration.java
@@ -1,7 +1,14 @@
 package com.example.real_visittogether;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import Event.Event1;
+import Event.Event2;
 
 public class Eventregistration extends AppCompatActivity {
 
@@ -9,5 +16,18 @@ public class Eventregistration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eventregistration);
+
+        Button button5 = (Button)findViewById(R.id.place);
+
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), placeAdd.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
