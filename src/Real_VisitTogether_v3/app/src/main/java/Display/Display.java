d44193bbc92ea341b318c6447e1cc899f43d2ac7
baek0307 <qorwkdgus93@gmail.com>
2019-04-07 02:18:57 +0900

    잘못올려서 수정해서 올림

--- src/Real_VisitTogether_v3/app/src/main/java/Display/Display.java
+++ src/Real_VisitTogether_v3/app/src/main/java/Display/Display.java
@@ -2,7 +2,10 @@ package Display;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 
@@ -19,15 +22,25 @@ public class Display extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display);
+
+        FloatingActionButton button4 = (FloatingActionButton)findViewById(R.id.ActionButton); //동그라미
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Eventregistration.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void onClick(View view) {
 
 
-        if(view.getId() == R.id.button1){
-            intent = new Intent(Display.this, Menu.class);
-            startActivity(intent);
-        }
+//        if(view.getId() == R.id.button1){
+//            intent = new Intent(Display.this, Menu.class);
+//            startActivity(intent);
+//        }
 
         if(view.getId() == R.id.button2){
             intent = new Intent(Display.this, Event1.class);
@@ -41,5 +54,10 @@ public class Display extends AppCompatActivity {
 
 
 
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_1, menu);
+        return true;
     }
 }
