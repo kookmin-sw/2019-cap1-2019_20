b9a5cb57fff104c53b31524566b769c298e43356
yangjeko <coyyjj7@naver.com>
2019-04-16 16:34:48 +0900

    Exif 인증페이지 추가
    Exif 인증완료/실패 페이지추가
    하지만 불러와진 메타데이터 값이 null로 뜸

---
+++ src/Real_VisitTogether_v3/app/src/main/java/authentication/SelectImage.java
@@ -0,0 +1,92 @@
+package authentication;
+
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.media.ExifInterface;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.TextView;
+import android.widget.Toast;
+import authentication.Auth_Exif;
+
+
+import com.example.real_visittogether.R;
+
+
+public class SelectImage extends AppCompatActivity {
+
+    private Intent intent;
+    private TextView photo_gps;
+    private TextView db_gps;
+    private boolean valid = false;
+    private String exifAttribute;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_image);
+
+        photo_gps = (TextView) findViewById(R.id.photo_gps);
+        db_gps = (TextView) findViewById(R.id.db_gps);
+
+        String path = ((Auth_Exif)Auth_Exif.context).currentPhotoPath;
+
+        try {
+            ExifInterface exif = new ExifInterface(path);
+            exifAttribute = getExif(exif);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(SelectImage.this, "Error", Toast.LENGTH_SHORT).show();
+        }
+
+        photo_gps.setText(exifAttribute);
+        db_gps.setText("db gps값");
+    }
+
+
+
+    public void onClickBack(View view) {
+
+        if (view.getId() == R.id.btnOK) {
+            intent = new Intent(SelectImage.this, authentication.SelectAuth.class);
+            startActivity(intent);
+        }
+
+    }
+    public void onClickAuth(View view) {
+
+        if (view.getId() == R.id.btnAuth) {
+            if (photo_gps == db_gps) {
+                Toast.makeText(SelectImage.this, "인증성공", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(SelectImage.this, "인증실패", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    private String getExif(ExifInterface exif) {
+        String myAttribute = "";
+        myAttribute += getTagString(ExifInterface.TAG_GPS_LATITUDE, exif);
+        myAttribute += getTagString(ExifInterface.TAG_GPS_LONGITUDE, exif);
+        return myAttribute;
+    }
+
+    private String getTagString(String tag, ExifInterface exif) {
+        return (tag + " : " + exif.getAttribute(tag) + "\n");
+    }
+
+}
