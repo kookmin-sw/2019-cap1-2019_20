a45817bc324f14e0f502a4e99673ee1034cc9e99
leehj8896 <leehj8896@kookmin.ac.kr>
2019-05-03 18:01:38 +0900

    각 객체 클래스 수정. 이벤트 등록 기능 추가

--- src/Real_VisitTogether_v3/app/src/main/java/data_fetcher/RequestHttpConnection.java
+++ src/Real_VisitTogether_v3/app/src/main/java/data_fetcher/RequestHttpConnection.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -49,6 +50,51 @@ public class RequestHttpConnection {
         }
 
         return result;
+    }
+
+    public String sendData(String event_name, String user_ID){
+
+        String postData = "event_name=" + event_name + "& user_ID=" + user_ID;
+
+        try {
+            URL url = new URL(strURL + "insert_event/");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestMethod("POST");
+            conn.setConnectTimeout(5000);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+
+            OutputStream outputStream = conn.getOutputStream();
+            outputStream.write(postData.getBytes("UTF-8"));
+            outputStream.flush();
+            outputStream.close();
+
+            String result = readStream(conn.getInputStream());
+            conn.disconnect();
+
+            return result;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private String readStream(InputStream in) throws IOException {
+        StringBuilder jsonHtml = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        String line = null;
+
+        while((line = reader.readLine()) != null)
+            jsonHtml.append(line);
+
+        reader.close();
+        return jsonHtml.toString();
     }
 }
 
