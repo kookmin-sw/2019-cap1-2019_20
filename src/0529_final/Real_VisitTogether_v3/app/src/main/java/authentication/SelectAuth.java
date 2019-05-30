070369b44a3805b58b688ffc28755af0d94a2986
baek0307 <qorwkdgus93@gmail.com>
2019-05-30 20:23:58 +0900

    장소별로 지정된 인증방식으로만 인증이 가능하도록 수정
    장소별로 지정된 인증방식으로만 인증이 가능하도록 수정  - SelectAuth내부 수정

--- src/0529_final/Real_VisitTogether_v3/app/src/main/java/authentication/SelectAuth.java
+++ src/0529_final/Real_VisitTogether_v3/app/src/main/java/authentication/SelectAuth.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.capstone.real_visittogether.R;
@@ -24,6 +25,7 @@ public class SelectAuth extends AppCompatActivity {
     private int auth_num;
     private int event_id;
     static String QR_Info ;
+    private int poss_exif ,poss_qr, poss_gps, poss_beacon;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,9 +38,75 @@ public class SelectAuth extends AppCompatActivity {
         event_id = getIntent().getIntExtra("event_id",-1);
         //place_id = intent.getIntExtra().getInt("place_id");
         new NetworkTask().execute();
+
+
+
+    }
+    public class NetworkTask extends AsyncTask<Void, Void, Void> {
+        String result;
+        String auth_info[];
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Register r = new Register();
+            result = r.select_auth_method(place_id);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            result = result.replaceAll("[^0-9]", "");
+
+            for(int i =0; i<result.length(); i++)
+            {
+
+                if(i ==0 &&result.charAt(i) == '1'){
+                    poss_qr =1;
+                    System.out.println("##################################################qr");
+
+                }
+                else if(i ==1 &&result.charAt(i) == '1'){
+                    poss_beacon =1;
+                    System.out.println("##################################################becoan");
+                }
+                else if(i ==2 &&result.charAt(i) == '1'){
+                    poss_exif =1;
+                    System.out.println("##################################################exif");
+                }
+                else if(i ==3 &&result.charAt(i) == '1'){
+                    poss_gps =1;
+                    System.out.println("##################################################gps");
+                }
+            }
+
+            Button auth_gps = (Button) findViewById(R.id.auth_gps);
+            Button auth_qr = (Button) findViewById(R.id.auth_qr);
+            Button auth_exif = (Button) findViewById(R.id.auth_exif);
+            Button auth_beacon = (Button) findViewById(R.id.auth_bicorn);
+
+
+            System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@poss_gps :"+poss_gps);
+            System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@##########################poss_qr :"+poss_qr);
+            System.out.println("#######################@@@@@@@@@@@@@@@@@@@@@@@@@###poss_exif :"+poss_exif);
+            System.out.println("##########################@@@@@@@@@@@@@@@@@@@@@@@@@poss_beacon :"+poss_beacon);
+
+            if(poss_gps !=1)
+                auth_gps.setVisibility(View.GONE);
+            if(poss_qr !=1)
+                auth_qr.setVisibility(View.GONE);
+            if(poss_exif !=1)
+                auth_exif.setVisibility(View.GONE);
+            if(poss_beacon !=1)
+                auth_beacon.setVisibility(View.GONE);
+
+        }
     }
 
+
+
+
     public void Select_Auth(View v) {
+
         if (v.getId() == R.id.auth_exif) {
             Intent intent = new Intent(SelectAuth.this, Auth_Exif.class);
             intent.putExtra("place_id", place_id);
@@ -137,57 +205,11 @@ public class SelectAuth extends AppCompatActivity {
                     qr_check check = new qr_check();
                     check.execute();
 
-                /*
-                if (true) {
-                    //if (QR_Info == result.getContents()) {
-                    Toast.makeText(SelectAuth.this, "인증성공", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(SelectAuth.this, "인증실패", Toast.LENGTH_SHORT).show();
-                }
-
-
-                Intent intent = new Intent(SelectAuth.this, Event1.class);
-                intent.putExtra("place_id", place_id);
-                intent.putExtra("authenticated", true);
-                startActivity(intent);
-                */
                 }
             }
         }
-    public class NetworkTask extends AsyncTask<Void, Void, Void> {
-        String result;
-        String auth_info[];
-        @Override
-        protected Void doInBackground(Void... voids) {
-            Register r = new Register();
-            result = r.select_auth_method(place_id);
-            return null;
-        }
 
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            result = result.replaceAll("[^0-9]", "");
 
-            for(int i =0; i<result.length(); i++)
-            {
 
-                if(i ==0 &&result.charAt(i) == '1'){
-
-                    System.out.println("##################################################qr");
-                }
-                else if(i ==1 &&result.charAt(i) == '1'){
-                    System.out.println("##################################################becoan");
-                }
-                else if(i ==2 &&result.charAt(i) == '1'){
-                    System.out.println("##################################################exif");
-                }
-                else if(i ==3 &&result.charAt(i) == '1'){
-                    System.out.println("##################################################gps");
-                }
-            }
-
-        }
-    }
     }
 
